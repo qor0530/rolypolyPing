bdc1ba21f077b1deffc06264b76e273ac58e505e
JH <jhk3261@gnu.ac.kr>
2024-11-17 13:48:07 +0900

    feat: UDP수신

---
+++ exempleProject/Assets/scripts/python_receiver.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class python_receiver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // udp수신기;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // data = udp수신기.recv()
+        // scores = data.scores;
+        // 3d_coords = data.3d_coords;
+
+        // for (int human_idx = 0; human_idx < scores.length; human_idx++)
+        // {
+        //     if scores[human_idx] >= 85:
+        //         excellent Ui 코드
+
+        // }
+
+        // Debug.Log(좌표: ㅌ, ㅌ, ㅌ)
+    }
+}
