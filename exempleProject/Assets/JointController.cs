7a50a4194062b1773b04dc669b1c2c088e83ed5f
JongHwa Paik <qorwhdghk12@gmail.com>
2024-11-24 20:51:39 +0900

    좌표계에 전환 확인.
    앞으로 해야할 일
    
    좌표-본 매핑
    관절 회전 처리
    애니메이션 여부 확인

--- exempleProject/Assets/JointController.cs
+++ exempleProject/Assets/JointController.cs
@@ -3,22 +3,51 @@ using UnityEngine;
 
 public class JointController : MonoBehaviour
 {
-    // 33개의 관절 Transform 리스트
-    public List<Transform> joints;
-
+    public List<Transform> joints; // Unity에서 사용하려는 본(관절) Transform
+    private int[] mappedLandmarks =
+    {
+        0,   // 머리
+        11,  // 오른쪽 어깨
+        12,  // 왼쪽 어깨
+        13,  // 오른쪽 팔꿈치
+        14,  // 왼쪽 팔꿈치
+        15,  // 오른쪽 손목
+        16,  // 왼쪽 손목
+        23,  // 오른쪽 골반
+        24,  // 왼쪽 골반
+        25,  // 오른쪽 무릎
+        26,  // 왼쪽 무릎
+        27,  // 오른쪽 발목
+        28   // 왼쪽 발목
+    };
     void Update()
     {
-        if (UDPReceiver.Instance != null && UDPReceiver.Instance.LatestCoord3D.Count == 33)
+        if (UDPReceiver.Instance != null && UDPReceiver.Instance.LatestCoord3D.Count >= mappedLandmarks.Length)
         {
             var coord3D = UDPReceiver.Instance.LatestCoord3D;
 
-            for (int i = 0; i < joints.Count; i++)
+            for (int i = 0; i < mappedLandmarks.Length; i++)
             {
-                var positionData = coord3D[i];
-                joints[i].localPosition = new Vector3(positionData[0], positionData[1], positionData[2]);
+                int landmarkIndex = mappedLandmarks[i];
+                var positionData = coord3D[landmarkIndex];
+
+                // 좌표 변환 및 스케일 조정
+                joints[i].localPosition = ConvertToUnityCoords(new Vector3(
+                    positionData[0], positionData[1], positionData[2]
+                ));
             }
 
-            Debug.Log("관절 위치가 업데이트되었습니다.");
+            Debug.Log("특정 본 업데이트 완료");
         }
+    }
+
+    private Vector3 ConvertToUnityCoords(Vector3 mediapipeCoord)
+    {
+        float scaleFactor = 5.0f; // 필요에 따라 스케일 조정
+        return new Vector3(
+            mediapipeCoord.x * scaleFactor,
+            mediapipeCoord.y * -scaleFactor, // Mediapipe의 z를 Unity의 y로 매핑
+            mediapipeCoord.z * scaleFactor  // Mediapipe의 y를 Unity의 z로 매핑
+        );
     }
 }
